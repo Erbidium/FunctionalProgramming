2594fd3a0864de2a7029235fc610afbf4d414bff
Oleksandr <OlexandrTanasienko@gmail.com>
2022-09-29 14:50:18 +0300

    Write first task of the lab

---
+++ DotNetTask/Lab1/Lab1/Functions.cs
@@ -0,0 +1,33 @@
+﻿namespace Lab1;
+
+public static class Functions
+{
+    public static List<T> Singleton<T>(T item) => new() { item };
+
+    public static bool Null<T>(List<T> list) => Length(list) == 0;
+
+    public static List<T> Snoc<T>(List<T> list, T newItem)
+    {
+        var length = Length(list);
+
+        var newArray = new T[length + 1];
+        for (var i = 0; i < length; i++)
+        {
+            newArray[i] = list[i];
+        }
+
+        newArray[length] = newItem;
+        return newArray.ToList();
+    }
+
+    public static int Length<T>(List<T> list)
+    {
+        var counter = 0;
+        foreach (var item in list)
+        {
+            counter++;
+        }
+
+        return counter;
+    }
+}
---
+++ DotNetTask/Lab1/Lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lab1;
+
+const int number = 5;
+var listFromNumber = Functions.Singleton(number);
+Console.WriteLine("Singleton function call");
+Console.WriteLine($"Singleton list element: {listFromNumber.First()}");
+Console.WriteLine();
+
+Console.WriteLine("Null function call");
+Console.WriteLine($"List is empty: {Functions.Null(listFromNumber)}");
+Console.WriteLine();
+
+Console.WriteLine("Snoc function call");
+Console.WriteLine("New lis after snoc call:");
+foreach (var listItem in Functions.Snoc(listFromNumber, number))
+{
+    Console.WriteLine(listItem);
+}
+Console.WriteLine();
+
+Console.WriteLine("Length function call");
+Console.WriteLine($"Length: {Functions.Length(listFromNumber)}");
+Console.WriteLine();
