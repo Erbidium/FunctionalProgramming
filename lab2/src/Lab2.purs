e90f9b35856f5d993ed83be6d83a0dd412bbb365
Oleksandr <OlexandrTanasienko@gmail.com>
2022-11-06 16:37:01 +0200

    Write zip function

--- lab2/src/Lab2.purs
+++ lab2/src/Lab2.purs
@@ -8,8 +8,10 @@ import Prelude
 
 import Data.List (List(Cons, Nil))
 import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Console (log)
+
 
 infixr 6 Cons as :
 
@@ -28,8 +30,15 @@ findLastIndex predicate (x : xs) = case findLastIndex predicate xs of
                                             else Nothing
 findLastIndex _ _ = Nothing
 
+zip :: forall a b. List a -> List b -> List (Tuple a b)
+zip (x1 : Nil) (x2 : _) = ((Tuple x1 x2) : Nil)
+zip (x1 : _) (x2 : Nil) = ((Tuple x1 x2) : Nil)
+zip (x1 : xs1) (x2 : xs2) = ((Tuple x1 x2) : (zip xs1 xs2))
+zip _ _ = Nil
+
 
 test::Effect Unit
 test = do
   log $ show $ findIndex (\x -> x == 5) (5: 5 : 5 : Nil)
   log $ show $ findLastIndex (\x -> x == 5) (5: 5 : 5 : Nil)
+  log $ show $ zip (1 : 2 : 3 : Nil) (1 : 2 : 3 : 4 : 5 : Nil)
