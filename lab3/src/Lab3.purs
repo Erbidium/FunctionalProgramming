134c5b32e7a0ba7f2dafdf5ca01b5db4302402e1
Oleksandr <OlexandrTanasienko@gmail.com>
2022-12-25 21:27:20 +0200

    Add starting code from lab for next tasks

--- lab3/src/Lab3.purs
+++ lab3/src/Lab3.purs
@@ -1,9 +1,11 @@
 module Lab3 where
 
-import Prelude (Unit, show, discard, (==), ($))
+import Prelude (Unit, show, discard, (==), ($), (<), (>), (<=), (>=))
 import Data.Eq (class Eq)
 import Effect (Effect)
 import Effect.Console (log)
+import Data.Ord (class Ord, Ordering(..), compare)
+import Data.Show (class Show)
 
 data Maybe a = Nothing | Just a
 
@@ -12,6 +14,16 @@ instance eqMaybe :: Eq a => Eq (Maybe a) where
   eq (Just x) (Just y) = x == y
   eq _ _ = false
 
+data Ordering = LT | GT | EQ
+
+compare :: a -> a -> Ordering
+
+greaterThanOrEq :: forall a. Ord a => a -> a -> Boolean
+
+
+infixl 4 greaterThanOrEq as >=
+
+
 test :: Effect Unit
 test = do
   log $ show $ Just 5 == Just 5
@@ -19,5 +31,16 @@ test = do
   log $ show $ Just 5 == Nothing
   log $ show $ Nothing == Just 5
   log $ show $ Nothing == (Nothing :: Maybe Unit)
+  log "------------------"
+  log $ show $ Just 1 < Just 5 -- COMPILER ERROR!!
+  log $ show $ Just 5 <= Just 5
+  log $ show $ Just 5 > Just 10
+  log $ show $ Just 10 >= Just 10
+  log $ show $ Just 99 > Nothing
+  log $ show $ Just 99 < Nothing
+  log $ show $ Just "abc"
+  log $ show $ (Nothing :: Maybe Unit)
+
+
 
 
