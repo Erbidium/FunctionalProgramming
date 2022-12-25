9706c02450536ad9adcad100fbc02b19f1b474e1
Oleksandr <OlexandrTanasienko@gmail.com>
2022-12-26 00:30:31 +0200

    Remove redundant code

--- lab3/src/Lab3.purs
+++ lab3/src/Lab3.purs
@@ -15,41 +15,11 @@ instance eqMaybe :: Eq a => Eq (Maybe a) where
   eq (Just x) (Just y) = x == y
   eq _ _ = false
 
-instance compareMaybe ::Ord Maybe where
+instance compareMaybe :: Ord a => Ord (Maybe a) where
   compare (Just a) (Just b) = compare a b
   compare (Just a) Nothing = GT
   compare Nothing (Just a) = LT
   compare Nothing Nothing = EQ
-
-
-
-greaterThanOrEq :: forall a. Ord a => a -> a -> Boolean
-greaterThanOrEq a1 a2 = case a1 `compare` a2 of
-  GT -> true
-  EQ -> true
-  _ -> false
-
-greaterThan :: forall a. Ord a => a -> a -> Boolean
-greaterThan a1 a2 = case a1 `compare` a2 of
-  GT -> true
-  _ -> false
-
-lessThanOrEq :: forall a. Ord a => a -> a -> Boolean
-lessThanOrEq a1 a2 = case a1 `compare` a2 of
-  LT -> true
-  EQ -> true
-  _ -> false
-
-lessThan :: forall a. Ord a => a -> a -> Boolean
-lessThan a1 a2 = case a1 `compare` a2 of
-  LT -> true
-  _ -> false
-
-infixl 4 greaterThanOrEq as >=
-infixl 4 greaterThan as >
-infixl 4 lessThanOrEq as <=
-infixl 4 lessThan as <
-
 
 test :: Effect Unit
 test = do
